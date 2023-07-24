b279696e3c753c10b5d1998dec8236563096d2fb
Pawandeep.Suryavanshi <pawandeep.suryavanshi@FVFJ626FQ6L4.bbrouter>
2023-07-25 01:58:20 +0530

    added function for calculating indicators using pandas

--- managers/analyse.py
+++ managers/analyse.py
@@ -3,6 +3,7 @@ import time
 from dateutil.relativedelta import relativedelta
 from sanic.response import text
 from sanic_ext import render
+import pandas as pd
 
 from utils import *
 from managers.stocks import read_stock, add_stock, remove_stock, update_preferences, get_preferences
@@ -178,3 +179,76 @@ def parse_time_series(data, duration):
     }
 
     return stock_data
+
+async def get_historical_data_df(candle_size, duration, stock):
+
+    candle_size_prefix, candle_size_suffix = break_string(candle_size)
+
+    function_type = period_function_mapping.get(candle_size_suffix, "TIME_SERIES_DAILY")
+    symbol = stock
+    interval = candle_size
+    url = URL.format(function_type, symbol, interval, ALPHA_VANTAGE_API_KEY)
+
+    try:
+        res = await make_request(url)
+    except Exception as e:
+        return (False, str(e))
+    
+    time_series_data = None
+
+    for key in list(res.keys()):
+        if "Time Series" in key:
+            time_series_data = res[key]
+
+    if time_series_data == None:
+        message = str(res)
+        print(message)
+        return (False, message)
+
+    data = res[list(res.keys())[1]]
+    data_df = pd.DataFrame(data).T
+
+    today_date = datetime.datetime.now().date()
+    num, chars = break_string(duration)
+    if chars == 'Y':
+        final_date = today_date + relativedelta(year=-1)
+    else:
+        final_date = today_date + relativedelta(months=-(int(num)))
+
+    start_date = final_date.strftime('%Y-%m-%d')
+    end_date = today_date.strftime('%Y-%m-%d')
+    data_df['4. close'] = data_df['4. close'].astype(float)
+
+    data_df = data_df[::-1]
+    data_df['MA'] = data_df['4. close'].rolling(7).mean()
+    data_df['RSI'] = await calculate_rsi(data_df['4. close'])
+    data_df['RSI'] = data_df['RSI'].fillna(0)
+    data_df['MA'] = data_df['MA'].fillna(0)
+    data_df = data_df[::-1]
+
+    data_df = data_df.loc[(data_df.index >= start_date) & (data_df.index <= end_date)]
+    min_close = data_df['4. close'].min()
+    max_close = data_df['4. close'].max()
+    avg_close = data_df['4. close'].mean()
+
+    stock_data = {
+        "symbol": stock,
+        "labels": data_df.index.tolist(),
+        "closing": data_df['4. close'].tolist(),
+        "min": float(min_close),
+        "max": float(max_close),
+        "average": float(avg_close),
+        "ma": data_df['MA'].tolist(),
+        "rsi": data_df['RSI'].tolist()
+    }
+    return True, stock_data
+
+async def calculate_rsi(prices, period=14):
+    delta = prices.diff().dropna()
+    gains = delta.where(delta > 0, 0)
+    losses = -delta.where(delta < 0, 0)
+    avg_gains = gains.rolling(window=period).mean()
+    avg_losses = losses.rolling(window=period).mean()
+    rs = avg_gains / avg_losses
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
